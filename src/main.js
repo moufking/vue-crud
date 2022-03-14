6f522f8b2db141d5227521c031572ef44efa3809
moufking <moufking337@gmail.com>
2022-03-14 10:27:28 +0100

    fix configuration boostrap

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
