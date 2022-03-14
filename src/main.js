645df6dc533b5b9d9a79732824179ede91e8316d
moufking <moufking337@gmail.com>
2022-03-14 10:15:02 +0100

    add boostrap in project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   render: h => h(App),
